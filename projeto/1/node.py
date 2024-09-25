86658382cddd15cd5d58a447d85d2e53b504d240
MateusKen <54600427+MateusKen@users.noreply.github.com>
2024-09-25 13:56:11 -0300

    Update node.py

--- projeto/1/node.py
+++ projeto/1/node.py
@@ -1,11 +1,17 @@
+"""
+NOME: Erik Samuel Viana Hsu
+RA: 10403109
+NOME: Mateus Kenzo Iochimoto
+RA: 10400995
+NOME: Thiago Shihan Cardoso Toma
+RA: 10400764
+"""
 
 class Node:
     
     def __init__(self, nome) -> None:
         self.nome = nome
         self.arestas = []
-        print(f"Node {nome} criado com sucesso")
     
     def insereAresta(self, aresta):
         self.arestas.append(aresta)
-        print(f"Aresta {aresta} inserida com sucesso em {id(self.arestas)}")
